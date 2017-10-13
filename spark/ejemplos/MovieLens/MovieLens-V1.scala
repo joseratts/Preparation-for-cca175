346746592fbf1bfd1a2f6732a18bfd485432d072
Jose Antonio Rodriguez <josearodriguezd@gmail.com>
2017-10-13 18:18:07 +0200

    fixup! solución al ejercicio elaborado con dataframe

--- spark/ejemplos/MovieLens/MovieLens-V1.scala
+++ spark/ejemplos/MovieLens/MovieLens-V1.scala
@@ -4,7 +4,7 @@ import sqlContext.implicits._
 import org.apache.spark.sql.functions._
 
 case class Rating (userId: String, movieId: String, rating: Float)
-case class Movie (movieId: String, title: String, genres: String)
+case class Movie (movieId: String, title: String, genres: Array[String])
 
 val ratingsFile = sc.textFile("file:///home/cloudera/Preparation-for-cca175/spark/ejemplos/MovieLens/data/ratings.csv")
 val header = ratingsFile.first() //extract header
@@ -16,18 +16,27 @@ val moviesFile = sc.textFile("file:///home/cloudera/Preparation-for-cca175/spark
 val moviesHeader = moviesFile.first() //extract header
 val moviesFileNoHeader = moviesFile.filter(row => row != moviesHeader)   //filter out header
 val moviesDF = moviesFileNoHeader.map{ line => val values = line.split(",")
-				Movie(values(0), values(1), values(2))}.toDF()
+				Movie(values(0), values(1), values(2).split("\\|"))}.toDF()
 
 val movieRatingDF = ratingDF.join(moviesDF, "movieId")
 
+//movieRatingDF.show()
+
 //Ejercicio 1
-movieRatingDF.select("movieId", "title", "rating").groupBy("movieId", "title").agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS")).filter($"REVIEWS" > 10).orderBy($"AVG-RATING".desc).show()
+movieRatingDF.select("movieId", "title", "rating").groupBy("movieId", "title")
+	.agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS")).filter($"REVIEWS" > 10)
+	.orderBy($"AVG-RATING".desc).show()
 
 //Ejercicio 2
-movieRatingDF.select("movieId", "title", "rating", "genres").where($"genres".contains("Comedy")).groupBy("movieId", "title").agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS")).filter($"REVIEWS" > 10).orderBy($"AVG-RATING".desc).show()
+movieRatingDF.select("movieId", "title", "rating", "genres").where(array_contains($"genres", "Comedy"))
+	.groupBy("movieId", "title").agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS"))
+	.filter($"REVIEWS" > 10).orderBy($"AVG-RATING".desc).show()
 
 //Ejercicio 3
-movieRatingDF.select("movieId", "title", "rating", "genres").where($"genres".contains("Drama")).groupBy("movieId", "title").agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS")).filter($"REVIEWS" > 10).orderBy($"AVG-RATING".desc).show()
+movieRatingDF.select("movieId", "title", "rating", "genres").where(array_contains($"genres","Drama"))
+	.groupBy("movieId", "title").agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS"))
+	.filter($"REVIEWS" > 10).orderBy($"AVG-RATING".desc).show()
 
 //Ejercicio 4
-ratingDF.select("userId", "rating").groupBy("userId").agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS")).orderBy($"REVIEWS".desc, $"AVG-RATING".desc).limit(20).show()
+ratingDF.select("userId", "rating").groupBy("userId").agg((avg("rating")).as("AVG-RATING"), (count("rating")).as("REVIEWS"))
+	.orderBy($"REVIEWS".desc, $"AVG-RATING".desc).limit(20).show()
