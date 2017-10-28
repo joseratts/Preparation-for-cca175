e5c1aed63617d12fdc50b4b16825681ab8621d59
Jose Antonio Rodriguez <josearodriguezd@gmail.com>
2017-10-28 11:25:46 -0300

    fixup! Ejercicio 4 del preparation plan de itversity

--- ejercicios itversity/ejercicio4/script.scala
+++ ejercicios itversity/ejercicio4/script.scala
@@ -17,7 +17,9 @@ textRDD.saveAsTextFile("/user/cloudera/problem4/text-gzip-compress", classOf[org
 //De la siguiente manera me da error
 //textRDD.saveAsTextFile("/user/cloudera/problem4/text-snappy-compress", classOf[org.apache.hadoop.io.compress.SnappyCodec])
 
-avroDF.map(x=> (x(0), x(1) + "\t" + x(2) + "\t" + x(3))).saveAsObjectFile("/user/cloudera/problem4/sequence")
+//Escribe el sequenceFile que no escribe los datos como tipo Text sino que lo hace como Object o ByteWritable
+//avroDF.map(x=> (x(0).toString, x(0) + "\t" + x(1) + "\t" + x(2) + "\t" + x(3))).saveAsObjectFile("/user/cloudera/problem4/sequence")
+avroDF.map(x=> (x(0).toString, x(0) + "\t" + x(1) + "\t" + x(2) + "\t" + x(3))).saveAsSequenceFile("/user/cloudera/problem4/sequence")
 
 //solucion problema 5
 val parquetSnappyDF = sqlContext.read.parquet("/user/cloudera/problem4/parquet-snappy-compress")
@@ -43,13 +45,18 @@ jsonGzipDF.show()
 jsonGzipDF.map(x=> x(0) + "," + x(1) + "," + x(2) + "," + x(3)).saveAsTextFile("/user/cloudera/problem4/csv-gzip", classOf[org.apache.hadoop.io.compress.GzipCodec])
 
 //solucion problema 8
-//Da error al intentar escribir
-/*
 val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
-val sequenceFileRDD = sc.sequenceFile("/user/cloudera/problem4/sequence", classOf[org.apache.hadoop.io.BytesWritable], classOf[org.apache.hadoop.io.BytesWritable])
+val sequenceFileRDD = sc.sequenceFile("/user/cloudera/problem4/sequence", classOf[org.apache.hadoop.io.Text], classOf[org.apache.hadoop.io.Text])
 val mappedRDD = sequenceFileRDD.map(x => {
     var fields = x._2.toString.split("\t"); (fields(0),fields(1),fields(2),fields(3))
   })
 val sequenceFileDF = hiveContext.createDataFrame(mappedRDD)
 sequenceFileDF.write.mode("overwrite").orc("/user/cloudera/problem4/orc");
-*/
+
+//Esta forma que es la utilizada por la gente de itversity me da error
+//java.lang.AssertionError: assertion failed: The ORC data source can only be used with HiveContext.
+/*val sequenceFileRDD = sc.sequenceFile("/user/cloudera/problem4/sequence", classOf[org.apache.hadoop.io.Text], classOf[org.apache.hadoop.io.Text])
+val sequenceFileDF = sequenceFileRDD.map(x => {
+    var fields = x._2.toString.split("\t"); (fields(0),fields(1),fields(2),fields(3))
+  }).toDF
+sequenceFileDF.write.orc("/user/cloudera/problem4/orc");*/
